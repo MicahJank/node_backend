6d82e4f98125d9ae4d3cc4bcd57b67a7a4dfc6b6
MicahJank <MicahJank@users.noreply.github.com>
2019-12-22 17:06:50 -0600

    made a file for the users model testing

---
+++ routes/auth/users-model.spec.js
@@ -0,0 +1,11 @@
+const Users = require('./users-model.js');
+const db = require('../../data/dbConfig.js');
+
+describe('users model testing', () => {
+
+    describe('find', () => {
+        it('should find and return a list off all the users in the database', () => {
+            
+        })
+    })
+})
