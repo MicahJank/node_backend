e8d1a19efec30ddd195f425c4fe4c61c72da431f
MicahJank <MicahJank@users.noreply.github.com>
2019-12-23 16:02:29 -0600

    added testing for the register router and finished building out the register route to make the tests pass

--- routes/auth/register-router.js
+++ routes/auth/register-router.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
+const Users = require('./users-model.js');
+
 const genToken = require('../genToken.js');
 
 // no need to add /register - that is already being used when we call router.use() in apiRouter
 router.post('/', (req, res) => {
-    // build out register router
+   const user = req.body;
 
-})
+   const hash = bcrypt.hashSync(user.password, 6);
+   user.password = hash;
+
+   Users.addUser(user)
+        .then(createdUser => {
+            const token = genToken(createdUser);
+
+            res.status(201).json({ created_user: createdUser, token });
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'An error occured while trying to register the user.', error: err });
+        });
+
+});
 
 
 module.exports = router;
--- routes/auth/register-router.spec.js
+++ routes/auth/register-router.spec.js
@@ -7,18 +7,34 @@ describe('register-router testing', () => {
 
     describe('registering a user', () => {
         it('should return 201 OK', () => {
-            // return db('users').truncate()
-            //         .then(() => {
-            //             return request(server).post('/api/register').send({ username: 'John', password: 'johnsnewpassword' })
-            //         })
+            return db('users').truncate()
+                    .then(() => {
+                        return request(server).post('/api/register').send({ username: 'John', password: 'johnsnewpassword' })
+                    })
+                    .then(res => {
+                        expect(res.status).toBe(201);
+                    });
         })
 
         it('should return a JSON object', () => {
-
+            return db('users').truncate()
+                    .then(() => {
+                        return request(server).post('/api/register').send({ username: 'John', password: 'johnsnewpassword' })
+                    })
+                    .then(res => {
+                        expect(res.type).toBe('application/json');
+                    });
         })
 
         it('should return a token in the body', () => {
-
+            return db('users').truncate()
+                    .then(() => {
+                        return request(server).post('/api/register').send({ username: 'John', password: 'johnsnewpassword' })
+                    })
+                    .then(res => {
+                        expect(res.body.token).toBeDefined();
+                        expect(typeof res.body.token).toBe('string');
+                    });
         })
     })
 })
